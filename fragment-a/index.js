5fbe7a002db52447fdc9499fc56788fd8cec4b75
dalongrong <1141591465@qq.com>
2018-12-10 14:21:49 +0800

    script tag load order test

--- fragment-a/index.js
+++ fragment-a/index.js
@@ -4,7 +4,7 @@ http
         res.writeHead(200, {
             'Content-Type': 'text/html'
         });
-        res.end('<div>Fragment 1</div>');
+        res.end(`<div>Fragment 1</div>`);
     })
     .listen(8081, function() {
         console.log('Fragment Server listening on port 8081');
--- fragment-b/index.js
+++ fragment-b/index.js
@@ -4,7 +4,9 @@ http
         res.writeHead(200, {
             'Content-Type': 'text/html'
         });
-        res.end('<div>Fragment 2</div>');
+        res.end(`<div>Fragment 2</div>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        `);
     })
     .listen(8082, function() {
         console.log('Fragment Server listening on port 8081');
